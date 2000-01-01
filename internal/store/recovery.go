e4cca1601858b0128e175b104976e3da35ac3c10
Benzhi Claimant <claimant@localhost>
2000-01-01 00:00:00 +0000

    Client result for 8c584b5a-ddec-4b06-80b3-556db55d23dc-bug-3

--- internal/store/recovery.go
+++ internal/store/recovery.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"fmt"
 	"os"
+	"reflect"
 
 	"manuscript-conservation-gate/internal/domain"
 )
@@ -20,6 +21,11 @@ func (s *Store) recover() error {
 	} else if snapshotErr != nil && !errors.Is(snapshotErr, os.ErrNotExist) {
 		// 损坏或过时的投影不是事实来源，下面依据已校验事件重建。
 	}
+	return s.rebuildFromLog(transactions, head)
+}
+
+// rebuildFromLog 依据已校验事件日志重建内存投影，并在存在事务时持久化新快照。
+func (s *Store) rebuildFromLog(transactions []transactionEnvelope, head string) error {
 	s.cases = map[string]*domain.ConservationCase{}
 	s.idempotency = map[string]domain.IdempotencyRecord{}
 	for _, transaction := range transactions {
@@ -35,21 +41,40 @@ func (s *Store) recover() error {
 	return nil
 }
 
+// validateProjection 将快照中的档案与幂等记录与已校验事件日志推导出的结果
+// 逐一核对。任何业务字段、嵌套集合或幂等记录的偏差都会触发从日志重建，
+// 从而保证 sequence、logHeadHash、档案 Version 和 AuditHeadHash 之外的字段
+// 被篡改时仍能恢复到事实来源。
 func (s *Store) validateProjection(transactions []transactionEnvelope) error {
-	latest := map[string]*domain.ConservationCase{}
-	for _, transaction := range transactions {
-		latest[transaction.Case.ID] = transaction.Case
-	}
-	if len(latest) != len(s.cases) {
+	expectedCases, expectedIdempotency := projectFromTransactions(transactions)
+	if len(expectedCases) != len(s.cases) || len(expectedIdempotency) != len(s.idempotency) {
 		return s.rebuildAfterProjectionMismatch(transactions)
 	}
-	for id, expected := range latest {
+	for id, expected := range expectedCases {
 		actual, ok := s.cases[id]
-		if !ok || actual.Version != expected.Version || actual.AuditHeadHash != expected.AuditHeadHash {
+		if !ok || !reflect.DeepEqual(expected, actual) {
+			return s.rebuildAfterProjectionMismatch(transactions)
+		}
+	}
+	for id, expected := range expectedIdempotency {
+		actual, ok := s.idempotency[id]
+		if !ok || !reflect.DeepEqual(expected, actual) {
 			return s.rebuildAfterProjectionMismatch(transactions)
 		}
 	}
 	return nil
+}
+
+// projectFromTransactions 依据事件事务序列推导出当前档案与幂等记录投影。
+// 返回的档案为日志中事务所持有实例的直接引用，仅供只读比对使用。
+func projectFromTransactions(transactions []transactionEnvelope) (map[string]*domain.ConservationCase, map[string]domain.IdempotencyRecord) {
+	cases := map[string]*domain.ConservationCase{}
+	idempotency := map[string]domain.IdempotencyRecord{}
+	for _, transaction := range transactions {
+		cases[transaction.Case.ID] = transaction.Case
+		idempotency[idempotencyID(transaction.Idempotency.Scope, transaction.Idempotency.Key)] = transaction.Idempotency
+	}
+	return cases, idempotency
 }
 
 func (s *Store) rebuildAfterProjectionMismatch(transactions []transactionEnvelope) error {
@@ -59,5 +84,18 @@ func (s *Store) rebuildAfterProjectionMismatch(transactions []transactionEnvelop
 		s.cases[transaction.Case.ID] = transaction.Case.Clone()
 		s.idempotency[idempotencyID(transaction.Idempotency.Scope, transaction.Idempotency.Key)] = transaction.Idempotency
 	}
-	return s.writeSnapshot()
+	s.sequence, s.headHash = int64(len(transactions)), latestHead(transactions)
+	if len(transactions) > 0 {
+		if err := s.writeSnapshot(); err != nil {
+			return fmt.Errorf("重建投影: %w", err)
+		}
+	}
+	return nil
+}
+
+func latestHead(transactions []transactionEnvelope) string {
+	if len(transactions) == 0 {
+		return ""
+	}
+	return transactions[len(transactions)-1].Hash
 }
